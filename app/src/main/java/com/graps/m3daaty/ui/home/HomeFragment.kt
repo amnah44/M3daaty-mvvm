032f1d3d6162854552e8c3434a256843be30b19d
basheer574 <spyderme99100@gmail.com>
2021-11-14 22:38:24 +0300

    home fragment done

---
+++ app/src/main/java/com/graps/m3daaty/ui/home/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.graps.m3daaty.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import com.graps.m3daaty.R
+import com.graps.m3daaty.databinding.HomeFragmentBinding
+import com.graps.m3daaty.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<HomeFragmentBinding>(R.layout.home_fragment) {
+    override val LOG_TAG: String
+        get() = TODO("Not yet implemented")
+    override val viewModel: ViewModel
+        get() = TODO("Not yet implemented")
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> HomeFragmentBinding
+        get() = TODO("Not yet implemented")
+
+    override fun setupView() {
+        TODO("Not yet implemented")
+    }
+}
