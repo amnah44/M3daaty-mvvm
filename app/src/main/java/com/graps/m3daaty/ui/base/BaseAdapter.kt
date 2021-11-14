a4f53a884ab9dc0f27294a4bf3185d48c733e6a1
basheer574 <spyderme99100@gmail.com>
2021-11-14 22:04:37 +0300

    add base fragment

---
+++ app/src/main/java/com/graps/m3daaty/ui/base/BaseAdapter.kt
@@ -0,0 +1,4 @@
+package com.graps.m3daaty.ui.base
+
+class BaseAdapter {
+}
---
+++ app/src/main/java/com/graps/m3daaty/ui/base/BaseFragment.kt
@@ -0,0 +1,48 @@
+package com.graps.m3daaty.ui.base
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.databinding.library.baseAdapters.BR
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.SavedStateViewModelFactory
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+abstract class BaseFragment<VDB : ViewDataBinding>(private val fragmentLayoutId: Int) : Fragment() {
+
+    override fun getDefaultViewModelProviderFactory(): ViewModelProvider.Factory =
+        SavedStateViewModelFactory(requireActivity().application, this, arguments ?: Bundle())
+
+    protected abstract val LOG_TAG: String
+    abstract val viewModel: ViewModel
+
+    private lateinit var _binding: VDB
+    val binding: VDB get() = _binding
+    abstract val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> VDB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater(inflater, fragmentLayoutId, container, false)
+        _binding.setVariable(BR.viewModel, viewModel)
+        _binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
+    abstract fun setupView()
+
+    protected fun log(value: Any) {
+        Log.v(LOG_TAG, value.toString())
+    }
+}
