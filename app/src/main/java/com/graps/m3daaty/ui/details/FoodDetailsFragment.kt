5ecc5d2d0bea1fa75734da78226682dc9befbbd2
basheer574 <spyderme99100@gmail.com>
2021-11-14 22:28:31 +0300

    fragment food details done

--- app/src/main/java/com/graps/m3daaty/ui/details/FoodDetailsFragment.kt
+++ app/src/main/java/com/graps/m3daaty/ui/details/FoodDetailsFragment.kt
@@ -1,4 +1,22 @@
 package com.graps.m3daaty.ui.details
 
-class FoodDetailsFragment {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import com.graps.m3daaty.R
+import com.graps.m3daaty.databinding.FragmentFoodDetailsBinding
+import com.graps.m3daaty.ui.base.BaseFragment
+
+class FoodDetailsFragment : BaseFragment<FragmentFoodDetailsBinding>(R.layout.fragment_food_details){
+    override val LOG_TAG: String
+        get() = TODO("Not yet implemented")
+    override val viewModel: ViewModel
+        get() = TODO("Not yet implemented")
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentFoodDetailsBinding
+        get() = TODO("Not yet implemented")
+
+    override fun setupView() {
+        TODO("Not yet implemented")
+    }
+
 }
