2fe73abef4d5ece3647e05f4ada32f7b3d16ac77
Amnah Ali <amnah1li@gmail.com>
2021-11-14 23:44:42 +0300

    fix error in base fragment

--- app/src/main/java/com/graps/m3daaty/ui/base/BaseFragment.kt
+++ app/src/main/java/com/graps/m3daaty/ui/base/BaseFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
-import androidx.databinding.library.baseAdapters.BR
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.lifecycle.ViewModel
