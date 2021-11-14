7a0a14018430e558dee74557976d8bae0127aa48
basheer574 <spyderme99100@gmail.com>
2021-11-14 22:06:34 +0300

    base adapter done

--- app/src/main/java/com/graps/m3daaty/ui/base/BaseAdapter.kt
+++ app/src/main/java/com/graps/m3daaty/ui/base/BaseAdapter.kt
@@ -1,4 +1,55 @@
 package com.graps.m3daaty.ui.base
 
-class BaseAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.databinding.library.baseAdapters.BR
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+
+interface BaseInteractionListener
+
+abstract class BaseAdapter<T>(
+    private var items: List<T>,
+    private var listener: BaseInteractionListener?
+) : RecyclerView.Adapter<BaseAdapter.BaseViewHolder>() {
+
+    abstract val layoutId : Int
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return ItemViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                layoutId,
+                parent,
+                false
+            ))
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        val currentItem = items[position]
+        when(holder) {
+            is ItemViewHolder ->{
+                holder.binding.setVariable(BR.item, currentItem)
+                holder.binding.setVariable(BR.listener, listener)
+            }
+        }
+    }
+
+    fun setItems(newItems : List<T>){
+        val moviesDiffUtil = DiffUtil.calculateDiff(FoodDiffUtils(items, newItems))
+        items = newItems
+        moviesDiffUtil.dispatchUpdatesTo(this)
+
+    }
+
+    fun getItems() = items
+
+    override fun getItemCount() = items.size
+
+    class ItemViewHolder(val binding: ViewDataBinding) : BaseViewHolder(binding)
+
+    abstract class BaseViewHolder(binding : ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+
 }
