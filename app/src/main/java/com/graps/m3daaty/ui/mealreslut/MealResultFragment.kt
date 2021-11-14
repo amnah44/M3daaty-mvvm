046a739754506f807600a4bc83cef7fb09070e6f
basheer574 <spyderme99100@gmail.com>
2021-11-14 22:32:02 +0300

    meal result fragment done

---
+++ app/src/main/java/com/graps/m3daaty/ui/mealreslut/MealResultFragment.kt
@@ -0,0 +1,21 @@
+package com.graps.m3daaty.ui.mealreslut
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import com.graps.m3daaty.R
+import com.graps.m3daaty.databinding.FragmentCategoryBinding
+import com.graps.m3daaty.ui.base.BaseFragment
+
+class MealResultFragment : BaseFragment<FragmentCategoryBinding>(R.layout.meal_result_fragment) {
+    override val LOG_TAG: String
+        get() = TODO("Not yet implemented")
+    override val viewModel: ViewModel
+        get() = TODO("Not yet implemented")
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentCategoryBinding
+        get() = TODO("Not yet implemented")
+
+    override fun setupView() {
+        TODO("Not yet implemented")
+    }
+}
