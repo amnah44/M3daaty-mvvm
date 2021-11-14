1bf53c67b83fd1ba7beaf48f27d086a384337fc9
basheer574 <spyderme99100@gmail.com>
2021-11-14 22:20:26 +0300

    category fragment done

---
+++ app/src/main/java/com/graps/m3daaty/ui/categoris/CategoryFragment.kt
@@ -0,0 +1,22 @@
+package com.graps.m3daaty.ui.categoris
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import com.graps.m3daaty.R
+import com.graps.m3daaty.databinding.FragmentCategoryBinding
+import com.graps.m3daaty.ui.base.BaseFragment
+
+class CategoryFragment : BaseFragment<FragmentCategoryBinding>(R.layout.fragment_category) {
+    override val LOG_TAG: String
+        get() = TODO("Not yet implemented")
+    override val viewModel: ViewModel
+        get() = TODO("Not yet implemented")
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentCategoryBinding
+        get() = TODO("Not yet implemented")
+
+    override fun setupView() {
+        TODO("Not yet implemented")
+    }
+
+}
