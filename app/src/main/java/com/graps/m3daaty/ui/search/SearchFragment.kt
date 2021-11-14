463de7a56303b6b15ce859dab00a680b534b6db1
basheer574 <spyderme99100@gmail.com>
2021-11-14 22:35:25 +0300

    search fragment done

--- app/src/main/java/com/graps/m3daaty/ui/search/SearchFragment.kt
+++ app/src/main/java/com/graps/m3daaty/ui/search/SearchFragment.kt
@@ -1,23 +1,21 @@
-package com.graps.m3daaty.ui.mealreslut
+package com.graps.m3daaty.ui.search
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModel
 import com.graps.m3daaty.R
-import com.graps.m3daaty.databinding.FragmentCategoryBinding
-import com.graps.m3daaty.databinding.MealResultFragmentBinding
+import com.graps.m3daaty.databinding.SearchFragmentBinding
 import com.graps.m3daaty.ui.base.BaseFragment
 
-class MealResultFragment : BaseFragment<MealResultFragmentBinding>(R.layout.meal_result_fragment) {
+class SearchFragment : BaseFragment<SearchFragmentBinding>(R.layout.search_fragment) {
     override val LOG_TAG: String
         get() = TODO("Not yet implemented")
     override val viewModel: ViewModel
         get() = TODO("Not yet implemented")
-    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> MealResultFragmentBinding
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> SearchFragmentBinding
         get() = TODO("Not yet implemented")
 
     override fun setupView() {
         TODO("Not yet implemented")
     }
-
 }
