a8499763193dda6899dfacac6f222ca623016152
MoeKraken <Mohammed27Zalzala11@gmail.com>
2021-11-15 07:21:17 +0300

    getrecipeinfo request method

--- app/src/main/java/com/graps/m3daaty/model/network/SpoonApiRequestMethods.kt
+++ app/src/main/java/com/graps/m3daaty/model/network/SpoonApiRequestMethods.kt
@@ -1,9 +1,11 @@
 package com.graps.m3daaty.model.network
 
+import com.graps.m3daaty.model.domain.getRecipeInformationResponse.RecipeInformation
 import com.graps.m3daaty.model.domain.randomRecipesResponse.RandomRecipes
 import com.graps.m3daaty.model.domain.recipeSearchResponse.RecipeSearch
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SpoonApiRequestMethods {
@@ -11,15 +13,21 @@ interface SpoonApiRequestMethods {
     @GET("random")
     fun getRandomRecipes(
         @Query("tags") filterTag: String?,
-        @Query("number") recipesCount: Int?,
+        @Query("number") recipesCount: Int?
     ) : Single<RandomRecipes>
 
     @GET("complexSearch")
     fun getRecipeSearchResult(
         @Query("query") recipeName:String?,
         @Query("cuisine") cuisine:String?,
-        @Query("number") recipesCount: Int?,
+        @Query("number") recipesCount: Int?
     ) : Single<RecipeSearch>
+
+    @GET("{id}/information")
+    fun getRecipeInfo(
+        @Path("id") recipeId:Int
+    ) : Single<RecipeInformation>
+
 
 
 }
