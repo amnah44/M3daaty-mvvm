7227c5f9b21b8500071a42f634790d17c75baf7e
basheer574 <spyderme99100@gmail.com>
2021-11-15 12:11:27 +0300

    add flag to lotti animation

--- app/src/main/java/com/graps/m3daaty/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/graps/m3daaty/ui/search/SearchViewModel.kt
@@ -1,7 +1,9 @@
 package com.graps.m3daaty.ui.search
 
+import androidx.lifecycle.MutableLiveData
 import com.graps.m3daaty.ui.base.BaseViewModel
 
 class SearchViewModel : BaseViewModel(){
+    val flag = MutableLiveData<Boolean>()
 
 }
