3c170e440185e37a4783f3746c3289c46fd9f174
Rafal A <rafal.akeel99@gmail.com>
2021-11-15 18:45:41 +0300

    fix activity xml name

--- app/src/main/java/com/graps/m3daaty/ui/M3daatyActivity.kt
+++ app/src/main/java/com/graps/m3daaty/ui/M3daatyActivity.kt
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.graps.m3daaty.R
-import com.graps.m3daaty.databinding.ActivityFoodBinding
+import com.graps.m3daaty.databinding.ActivityM3daatyBinding
 
 class M3daatyActivity : AppCompatActivity() {
-    private lateinit var _binding: ActivityFoodBinding
+    private lateinit var _binding: ActivityM3daatyBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = DataBindingUtil.setContentView(this, R.layout.activity_food)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_m3daaty)
     }
 }
