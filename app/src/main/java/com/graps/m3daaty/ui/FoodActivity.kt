9817e683bb572c531a511f82b07b3687642cbdcc
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-15 21:42:16 +0300

    add on resume fun to food activity

--- app/src/main/java/com/graps/m3daaty/ui/FoodActivity.kt
+++ app/src/main/java/com/graps/m3daaty/ui/FoodActivity.kt
@@ -3,6 +3,8 @@ package com.graps.m3daaty.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.graps.m3daaty.R
 import com.graps.m3daaty.databinding.ActivityFoodBinding
 
@@ -12,5 +14,11 @@ class FoodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_food)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val navController = findNavController(R.id.parent_container)
+        _binding.bottomNav.setupWithNavController(navController)
     }
 }
