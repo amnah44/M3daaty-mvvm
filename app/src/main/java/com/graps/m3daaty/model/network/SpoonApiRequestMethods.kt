e4a305270c49b4e0b13520c9640c723cce41b5fb
MoeKraken <Mohammed27Zalzala11@gmail.com>
2021-11-15 07:18:02 +0300

    add getrecipesearchresult request method

--- app/src/main/java/com/graps/m3daaty/model/network/SpoonApiRequestMethods.kt
+++ app/src/main/java/com/graps/m3daaty/model/network/SpoonApiRequestMethods.kt
@@ -1,18 +1,25 @@
 package com.graps.m3daaty.model.network
 
 import com.graps.m3daaty.model.domain.randomRecipesResponse.RandomRecipes
+import com.graps.m3daaty.model.domain.recipeSearchResponse.RecipeSearch
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface SpoonApiRequestMethods {
 
-    @GET("recipes/random")
+    @GET("random")
     fun getRandomRecipes(
         @Query("tags") filterTag: String?,
-        @Query("number") recipesCount: Int
+        @Query("number") recipesCount: Int?,
     ) : Single<RandomRecipes>
 
+    @GET("complexSearch")
+    fun getRecipeSearchResult(
+        @Query("query") recipeName:String?,
+        @Query("cuisine") cuisine:String?,
+        @Query("number") recipesCount: Int?,
+    ) : Single<RecipeSearch>
 
 
 }
