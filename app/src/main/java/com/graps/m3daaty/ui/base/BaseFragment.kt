b768b0fb7abf6a0270708330d945ca5b75d3df39
Amnah Ali <amnah1li@gmail.com>
2021-11-15 13:31:33 +0300

    refactor base fragment

--- app/src/main/java/com/graps/m3daaty/ui/base/BaseFragment.kt
+++ app/src/main/java/com/graps/m3daaty/ui/base/BaseFragment.kt
@@ -7,17 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.SavedStateViewModelFactory
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 
 abstract class BaseFragment<VDB : ViewDataBinding>(private val fragmentLayoutId: Int) : Fragment() {
 
-    override fun getDefaultViewModelProviderFactory(): ViewModelProvider.Factory =
-        SavedStateViewModelFactory(requireActivity().application, this, arguments ?: Bundle())
-
     protected abstract val LOG_TAG: String
-    abstract val viewModel: ViewModel
+    abstract val viewModel: BaseViewModel
 
     private lateinit var _binding: VDB
     val binding: VDB get() = _binding
@@ -29,13 +23,12 @@ abstract class BaseFragment<VDB : ViewDataBinding>(private val fragmentLayoutId:
         savedInstanceState: Bundle?
     ): View? {
         _binding = bindingInflater(inflater, fragmentLayoutId, container, false)
-//        _binding.setVariable(BR.viewModel, viewModel)
-        _binding.lifecycleOwner = this
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        
         setupView()
     }
 
