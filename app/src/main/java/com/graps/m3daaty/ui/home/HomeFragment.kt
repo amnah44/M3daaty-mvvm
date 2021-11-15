01041854bd60bf830c46155609439034b978fd16
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-15 21:55:34 +0300

    refactor home fragment

--- app/src/main/java/com/graps/m3daaty/ui/home/HomeFragment.kt
+++ app/src/main/java/com/graps/m3daaty/ui/home/HomeFragment.kt
@@ -2,6 +2,8 @@ package com.graps.m3daaty.ui.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import com.graps.m3daaty.R
 import com.graps.m3daaty.databinding.FragmentHomeBinding
 import com.graps.m3daaty.ui.base.BaseFragment
@@ -9,12 +11,11 @@ import com.graps.m3daaty.ui.base.BaseFragment
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override val LOG_TAG: String
         get() = TODO("Not yet implemented")
-    override val viewModel: HomeViewModel
-        get() = TODO("Not yet implemented")
-    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentHomeBinding
-        get() = TODO("Not yet implemented")
+    override val viewModel by activityViewModels<HomeViewModel>()
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentHomeBinding =
+        DataBindingUtil :: inflate
 
     override fun setupView() {
-        TODO("Not yet implemented")
+        
     }
 }
