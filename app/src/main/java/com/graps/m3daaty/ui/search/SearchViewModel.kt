a071b56411bbe3f395d3f77f3f9383b2fd5010e2
basheer574 <spyderme99100@gmail.com>
2021-11-15 12:18:30 +0300

    add flag to search view model

--- app/src/main/java/com/graps/m3daaty/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/graps/m3daaty/ui/search/SearchViewModel.kt
@@ -1,9 +1,12 @@
 package com.graps.m3daaty.ui.search
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.graps.m3daaty.ui.base.BaseViewModel
 
 class SearchViewModel : BaseViewModel(){
-    val flag = MutableLiveData<Boolean>()
+    private val _flag = MutableLiveData<Boolean>()
+    val flag: LiveData<Boolean> get() = _flag
+
 
 }
