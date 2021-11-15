287c88bf970a6729d2f776be59dc51698f784f6a
MoeKraken <Mohammed27Zalzala11@gmail.com>
2021-11-15 07:24:50 +0300

    add getrecipenutration request method

--- app/src/main/java/com/graps/m3daaty/model/network/SpoonApiRequestMethods.kt
+++ app/src/main/java/com/graps/m3daaty/model/network/SpoonApiRequestMethods.kt
@@ -2,6 +2,7 @@ package com.graps.m3daaty.model.network
 
 import com.graps.m3daaty.model.domain.getRecipeInformationResponse.RecipeInformation
 import com.graps.m3daaty.model.domain.randomRecipesResponse.RandomRecipes
+import com.graps.m3daaty.model.domain.recipeNutritionReponse.RecipeNutrition
 import com.graps.m3daaty.model.domain.recipeSearchResponse.RecipeSearch
 import com.graps.m3daaty.model.domain.tasteResponse.RecipeTaste
 import io.reactivex.rxjava3.core.Single
@@ -34,6 +35,9 @@ interface SpoonApiRequestMethods {
         @Path("id") recipeId:Int
     ) : Single<RecipeTaste>
 
-
+    @GET("{id}/nutritionWidget.json")
+    fun getRecipeNutrition(
+        @Path("id") recipeId:Int
+    ) : Single<RecipeNutrition>
 
 }
