c6555c4b61bb63609878ee4ba888714891a3485d
Amnah Ali <amnah1li@gmail.com>
2021-11-15 18:33:41 +0300

    remove unusable import

--- app/src/main/java/com/graps/m3daaty/model/network/SpoonacularApi.kt
+++ app/src/main/java/com/graps/m3daaty/model/network/SpoonacularApi.kt
@@ -1,12 +1,10 @@
 package com.graps.m3daaty.model.network
 
 import com.graps.m3daaty.util.Constants
-import io.reactivex.rxjava3.plugins.RxJavaPlugins
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 object SpoonacularApi {
 
