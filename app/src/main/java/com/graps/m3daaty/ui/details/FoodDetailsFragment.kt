953dc2082b8b44728fcd40507916efa65294828e
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-16 17:41:51 +0300

    setup values of food details fragment

--- app/src/main/java/com/graps/m3daaty/ui/details/FoodDetailsFragment.kt
+++ app/src/main/java/com/graps/m3daaty/ui/details/FoodDetailsFragment.kt
@@ -2,18 +2,18 @@ package com.graps.m3daaty.ui.details
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModel
 import com.graps.m3daaty.R
 import com.graps.m3daaty.databinding.FragmentFoodDetailsBinding
 import com.graps.m3daaty.ui.base.BaseFragment
+import com.graps.m3daaty.util.Constants
 
 class FoodDetailsFragment : BaseFragment<FragmentFoodDetailsBinding>(R.layout.fragment_food_details){
-    override val LOG_TAG: String
-        get() = TODO("Not yet implemented")
-    override val viewModel: FoodDetailsViewModel
-        get() = TODO("Not yet implemented")
-    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentFoodDetailsBinding
-        get() = TODO("Not yet implemented")
+    override val LOG_TAG: String=Constants.FOOD_DETAILS_FRAGMENT
+    override val viewModel: FoodDetailsViewModel by activityViewModels()
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentFoodDetailsBinding=DataBindingUtil::inflate
 
     override fun setupView() {
         TODO("Not yet implemented")
--- app/src/main/java/com/graps/m3daaty/util/Constants.kt
+++ app/src/main/java/com/graps/m3daaty/util/Constants.kt
@@ -4,5 +4,6 @@ object Constants {
 
     const val Base_URL = " https://api.spoonacular.com/recipes/"
     const val apiKey = "8f09860e23a549f18f24e73f0f36b3fd"
+    const val FOOD_DETAILS_FRAGMENT="food details fragment"
 
 }
