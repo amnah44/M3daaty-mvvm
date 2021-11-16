16c3bb9acc95fb35e95333dd46f4cd827e062cc7
Mohammed Ali <mohammed.mizban97@gmail.com>
2021-11-16 17:55:26 +0300

    setup ingredients interaction listener

---
+++ app/src/main/java/com/graps/m3daaty/ui/details/FoodInteractionListener.kt
@@ -0,0 +1,7 @@
+package com.graps.m3daaty.ui.details
+
+import com.graps.m3daaty.ui.base.BaseInteractionListener
+
+interface IngredientsInteractionListener:BaseInteractionListener{
+    fun onClickIngredient(ingredientsText:String)
+}
