ed389907606455c274bbab079be9ff49a3248eb6
basheer574 <spyderme99100@gmail.com>
2021-11-16 15:02:39 +0300

    search adapter done search listener done

--- app/src/main/java/com/graps/m3daaty/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/graps/m3daaty/ui/search/SearchAdapter.kt
@@ -7,7 +7,7 @@ import com.graps.m3daaty.R
 import com.graps.m3daaty.model.domain.recipeSearch.Result
 import com.graps.m3daaty.ui.base.BaseAdapter
 
-class SearchAdapter(val searchItems: List<Result>, listener: SearchInteractionListener) :
+class SearchAdapter(searchItems: List<Result>, listener: SearchInteractionListener) :
     BaseAdapter<Result>(searchItems, listener) {
     override val layoutId: Int
         get() = R.layout.item_search
