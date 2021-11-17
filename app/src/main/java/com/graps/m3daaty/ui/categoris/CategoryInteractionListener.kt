f35767b7f0ebe9e757a017aac3f55c1c45e493ff
Rafal A <rafal.akeel99@gmail.com>
2021-11-17 15:12:23 +0300

    fix category listener

--- app/src/main/java/com/graps/m3daaty/ui/categoris/CategoryInteractionListener.kt
+++ app/src/main/java/com/graps/m3daaty/ui/categoris/CategoryInteractionListener.kt
@@ -2,7 +2,9 @@ package com.graps.m3daaty.ui.categoris
 
 import com.graps.m3daaty.model.domain.recipeSearch.RecipeSearch
 import com.graps.m3daaty.ui.base.BaseInteractionListener
+import com.graps.m3daaty.util.CuisineList
+import com.graps.m3daaty.util.State
 
 interface CategoryInteractionListener:BaseInteractionListener {
-    fun onCategorySelected(cuisine:RecipeSearch)
+    fun onCategorySelected(cuisine: State<CuisineList>)
 }
