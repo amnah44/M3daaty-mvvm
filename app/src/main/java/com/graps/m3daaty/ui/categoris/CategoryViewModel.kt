545e3816cda3a2a17deab6c4d7d750e060b6fb62
Rafal A <rafal.akeel99@gmail.com>
2021-11-17 15:19:15 +0300

    add functionality to category view model

--- app/src/main/java/com/graps/m3daaty/ui/categoris/CategoryViewModel.kt
+++ app/src/main/java/com/graps/m3daaty/ui/categoris/CategoryViewModel.kt
@@ -1,8 +1,31 @@
 package com.graps.m3daaty.ui.categoris
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.graps.m3daaty.model.domain.recipeSearch.RecipeSearch
+import com.graps.m3daaty.model.repository.Repository.getRecipeSearchResult
 import com.graps.m3daaty.ui.base.BaseViewModel
+import com.graps.m3daaty.util.*
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
 
-class CategoryViewModel : BaseViewModel() {
+class CategoryViewModel : BaseViewModel(),CategoryInteractionListener {
 
+    private val _category = MutableLiveData<List<CuisineList>>()
+    val category:LiveData<List<CuisineList>> get()= _category
+    val s = CuisineList()
+
+
+    init {
+        cuisineCategory()
+    }
+    override fun onCategorySelected(cuisine: State<CuisineList>) {
+
+    }
+
+    private fun cuisineCategory(){
+        _category.postValue(category.value)
+    }
 
 }
