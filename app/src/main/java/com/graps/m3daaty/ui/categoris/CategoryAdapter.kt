9784a6ab75e0ab7bec1353b71823ea45d8fa8e3e
Rafal A <rafal.akeel99@gmail.com>
2021-11-17 15:11:30 +0300

    fix category adapter

--- app/src/main/java/com/graps/m3daaty/ui/categoris/CategoryAdapter.kt
+++ app/src/main/java/com/graps/m3daaty/ui/categoris/CategoryAdapter.kt
@@ -4,8 +4,9 @@ import com.graps.m3daaty.R
 import com.graps.m3daaty.model.domain.recipeSearch.Result
 import com.graps.m3daaty.ui.base.BaseAdapter
 import com.graps.m3daaty.util.Category
+import com.graps.m3daaty.util.CuisineList
 
-class CategoryAdapter(list:List<Category>,listener:CategoryInteractionListener):BaseAdapter<Category>(list,listener) {
+class CategoryAdapter(list:List<CuisineList>,listener:CategoryInteractionListener?):BaseAdapter<CuisineList>(list,listener) {
     override val layoutId: Int
         get() = R.layout.item_cuisine
 }
