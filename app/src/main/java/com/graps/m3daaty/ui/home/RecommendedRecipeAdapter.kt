3e8086ab25ee3b0499cdbf441a217dbb81bf8f4b
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-18 22:30:56 +0300

    add recommended interaction listener

---
+++ app/src/main/java/com/graps/m3daaty/ui/home/RecommendedRecipeAdapter.kt
@@ -0,0 +1,8 @@
+package com.graps.m3daaty.ui.home
+
+import com.graps.m3daaty.ui.base.BaseInteractionListener
+
+class RecommendedRecipeAdapter {
+}
+
+interface RecommendedInteractionListener: BaseInteractionListener
