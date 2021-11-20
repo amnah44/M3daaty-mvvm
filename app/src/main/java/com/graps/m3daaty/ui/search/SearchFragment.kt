5b95c43f63b396ace6be38240355e3c41e1bc1e6
basheer574 <spyderme99100@gmail.com>
2021-11-20 16:10:46 +0300

    add shimmer effect

--- app/src/main/java/com/graps/m3daaty/ui/search/SearchFragment.kt
+++ app/src/main/java/com/graps/m3daaty/ui/search/SearchFragment.kt
@@ -1,13 +1,13 @@
 package com.graps.m3daaty.ui.search
 
+
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import com.graps.m3daaty.R
 import com.graps.m3daaty.databinding.FragmentSearchBinding
-import com.graps.m3daaty.model.domain.recipeSearch.Result
 import com.graps.m3daaty.ui.base.BaseFragment
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
@@ -16,7 +16,11 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
         DataBindingUtil::inflate
 
     override fun setupView() {
+        binding.shimmerLayout.startShimmer()
+        binding.shimmerLayout.startShimmer()
         binding.viewModel = viewModel
         binding.searchRecyclerView.adapter = SearchAdapter(mutableListOf(),viewModel)
+        binding.shimmerLayout.stopShimmer()
+        binding.shimmerLayout.visibility = View.GONE
     }
 }
