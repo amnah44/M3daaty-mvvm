d948a07955040b0dc59aa5c324e0487cf36177c7
Moe Zalzala <Mohammed27Zalzala11@gmail.com>
2021-11-21 12:57:55 +0300

    don't give a damn fuck anymore

--- app/src/main/java/com/graps/m3daaty/model/network/Client.kt
+++ app/src/main/java/com/graps/m3daaty/model/network/Client.kt
@@ -16,7 +16,7 @@ object Client {
 
             val originalHttpUrl = original.url
                 .newBuilder()
-                .addQueryParameter("apiKey", Constants.apiKey)
+                .addQueryParameter("apiKey", Constants.BASHIR_API_KEY)
                 .build()
 
             val request = original.newBuilder().url(originalHttpUrl).build()
