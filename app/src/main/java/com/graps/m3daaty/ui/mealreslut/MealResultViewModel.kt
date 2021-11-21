41e09537f90cb16098080c390ec89d58539e469d
Moe Zalzala <Mohammed27Zalzala11@gmail.com>
2021-11-21 11:18:49 +0300

    create MealResultViewModel

--- app/src/main/java/com/graps/m3daaty/ui/mealreslut/MealResultViewModel.kt
+++ app/src/main/java/com/graps/m3daaty/ui/mealreslut/MealResultViewModel.kt
@@ -1,29 +1,42 @@
 package com.graps.m3daaty.ui.mealreslut
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.graps.m3daaty.model.domain.randomRecipes.RandomRecipes
+import com.graps.m3daaty.model.domain.recipeSearch.RecipeSearch
 import com.graps.m3daaty.model.network.Api
 import com.graps.m3daaty.model.repository.Repository
 import com.graps.m3daaty.ui.base.BaseViewModel
-import com.graps.m3daaty.util.Constants
-import com.graps.m3daaty.util.Cuisine
-import com.graps.m3daaty.util.CuisineList
-import com.graps.m3daaty.util.State
+import com.graps.m3daaty.util.*
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MealResultViewModel : BaseViewModel() {
 
-    val certainCuisineRecipe = MutableLiveData<String>()
+    private val _certainCuisineRecipe = MutableLiveData<State<RecipeSearch>>()
+
+    val certainCuisineRecipe: LiveData<State<RecipeSearch>>
+        get() = _certainCuisineRecipe
 
     init {
         getRecipes()
     }
 
     private fun getRecipes(){
-
+        _certainCuisineRecipe.postValue(State.Loading)
+        observe(Repository.getRecipeSearchResult(
+            null,
+            Category.ITALIAN ),
+            ::onSuccess ,
+            ::onError
+        )
 
     }
+
+    private fun onSuccess(recipes : State<RecipeSearch>) =
+        _certainCuisineRecipe.postValue(recipes)
+
+    private fun onError(throwable: Throwable) = State.Error(throwable.message.toString())
 }
